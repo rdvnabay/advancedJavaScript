f67dfa529e25e2cbbc6647368dc0d51fc85eec72
Rıdvan <ridvanabay@gmail.com>
2021-06-02 22:41:46 +0300

    10. Gün - 1.Ara

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let sayi1=10
+sayi1="İshak"
+let student={
+    id:1,
+    name:"İbrahim"
+}
+console.log(student)
+
+function save(puan=10,ogrenci) {
+    console.log(ogrenci.name+" : "+puan)
+}
+
+save(undefined,100)
